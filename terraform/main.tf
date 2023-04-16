32f6d27ba12afc5c53473ed43dfcd40f4e853f84
snipesa <etunyiashime@gmail.com>
2023-04-16 08:50:43 +0100

    update state back end

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-west-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "snipes-github-bucket1"
+    key = "terraform.tfstate"   # created in console if file location available
+    region = "us-west-1"
+  }
+}
+
 resource "aws_ecr_repository" "repo1" {
   name                 = "app-repo"
   image_tag_mutability = "MUTABLE"
