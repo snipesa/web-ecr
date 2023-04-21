caa6a019925ac5fef7367db70ab6c6f8160f23e8
snipesa <etunyiashime@gmail.com>
2023-04-22 00:09:57 +0100

    terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "vpc_cidr" {
+
+}
+
+variable "cidr_priv1" {
+}
+
+variable "cidr_priv2" {
+}
+
+variable "cidr_priv3" {
+}
+
+variable "cidr_pub1" {
+}
+
+variable "cidr_pub2" {
+
+}
+
+variable "cidr_pub3" {
+}
+
+variable "az_1" {
+}
+
+variable "az_2" {
+}
+
+variable "az_3" {
+}
+
+variable "sg-web" {
+}
